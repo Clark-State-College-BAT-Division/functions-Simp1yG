973f6dc51eb133488758aab2c382efd5a3f47e0a
Simp1yG <bgrantthurman@gmail.com>
2026-03-09 22:14:06 -0400

    Update rolldice.py

--- rolldice.py
+++ rolldice.py
@@ -19,3 +19,32 @@
 # How many sides? 20
 # Here are the results: [18, 19, 6, 8, 13, 6, 6, 6, 18, 12, 20, 10, 14, 8, 14, 17, 12, 15, 20, 17]
 
+def Dice_Town_Baby():
+    import random
+    while True:
+        try:
+            num_dice = int(input("How many dice to roll? "))
+            if num_dice < 1:
+                print("You have to roll at least one die.")
+                continue
+            break
+        except:
+            print("Please choose a number.")
+    while True:
+        try:
+            num_sides = int(input("How many sides? "))
+            if num_sides < 2:
+                print("The number of sides must be greater than 1.")
+                continue
+            break
+        except:
+            print("Please choose a number.")
+    rolls = []
+    roll_total = 0
+    for d in range(num_dice):
+        dice_roll = random.randint(1, num_sides)
+        roll_total = roll_total + dice_roll
+        rolls.append(dice_roll)
+    print(f"You rolled {rolls}\nfor a total of {roll_total}.")
+
+Dice_Town_Baby()
